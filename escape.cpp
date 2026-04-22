ad8917f0b6739205583d67f019ac59ac0a5dea98
s1140533-wq <s1140533@dmhs.kh.edu.tw>
2026-04-22 15:16:02 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+  count << "1快樂星球 " << endl;
+  count << "2快樂 星球 " << endl;
+  count << "3快樂\t星球 " << endl;
+  count << "4快樂\n星球 " << endl;
+  count << "\\5快樂星球\\ " << endl;
+  count << "\"6快樂星球\"" << endl; 
+  return 0;
+}
