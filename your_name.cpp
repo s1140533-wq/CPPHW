46c5556f24e6b4f8569c3acff0bf325a82cabd69
s1140533-wq <s1140533@dmhs.kh.edu.tw>
2026-04-27 09:56:15 +0800

    Create  your_name.cpp

---
+++ your_name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int a, b;
+    cout << "輸入姓名\n"
+    cin >> name;
+    cout <<"hello," << name <<endl;
+    cont <<"輸入a空格b\n";
+    cin >> a >> b;
+    cont << "a是" << a << "b是" << b; 
+  	return 0;
